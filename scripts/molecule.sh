3263ed640402b13ee6da16f9f260e2527b20b4c6
Anton Kachurin <anton.kachurin@t-systems.com>
2020-12-29 16:41:36 +0300

    Add bash script logging

--- scripts/molecule.sh
+++ scripts/molecule.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/bash
+echo "Starting molecule test"
 
 project_root=$(realpath "$(dirname "$0")")
+echo "Project root: $project_root"
 
 # select testable role_paths
 role_paths=$(find "$project_root/roles" -maxdepth 2 -name "molecule" -not -path "*/.tox*")
+echo "Found testable roles at: $role_paths"
 
 rc=0
-
 for role_path in $role_paths; do
   role_path=$(realpath "$role_path/..")
   echo "Role at $role_path"
