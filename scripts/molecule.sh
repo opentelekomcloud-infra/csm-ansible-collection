8a6981de6923dadd84a98d0d2385e0b7cd141de3
Anton Kachurin <anton.kachurin@t-systems.com>
2020-12-29 16:33:05 +0300

    Fix `molecule.sh`

--- scripts/molecule.sh
+++ scripts/molecule.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/bash
 
+project_root=$(realpath "$(dirname "$0")")
+
 # select testable role_paths
-role_paths=$(find "$PWD/roles" -maxdepth 2 -name "molecule" -not -path "*/.tox*")
+role_paths=$(find "$project_root/roles" -maxdepth 2 -name "molecule" -not -path "*/.tox*")
 
 rc=0
 
 for role_path in $role_paths; do
-  role_path="$role_path/.."
+  role_path=$(realpath "$role_path/..")
+  echo "Role at $role_path"
   cd "$role_path" || exit 2
   echo "Start test at $PWD"
   molecule test || rc=1
